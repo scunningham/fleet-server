e0630ae5628e1ae74d42c4fad6d8a059d1e4691c
Sean Cunningham <sean.cunningham@elastic.co>
2021-04-19 11:30:30 -0400

    comments

--- internal/pkg/bulk/block.go
+++ internal/pkg/bulk/block.go
@@ -4,15 +4,16 @@
 
 package bulk
 
-import ()
+// bulkT is generally allocated in the bulk engines's 'blkPool'
+// However, the multiOp API's will allocate directly in large blocks.
 
 type bulkT struct {
-	idx    int
-	action Action
-	ch     chan respT
-	buf    Buf
-	opts   optionsT
-	next   *bulkT
+	idx    int        // idx of originating requeset, used in mulitOp
+	action Action     // requested actions
+	ch     chan respT // response channel, caller is waiting synchronously
+	buf    Buf        // json payload to be sent to elastic
+	opts   optionsT   // various options
+	next   *bulkT     // pointer to next bulkT, used for fast internal queueing
 }
 
 func (blk *bulkT) reset() {
